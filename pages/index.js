b6d99a33b7e8e6cdba74df2788c7b0cc225f2159
Ally Juma Said <mahadsaid95@icloud.com>
2023-01-04 19:13:23 +0000

    Adding a CSS module,and using it for page styling anytime with any page

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,19 @@ import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-      <h1>Hello NextJs</h1>
-      <p>welcome to our sitution gderwe sedcgfioyitkhftuo  yhghg going bt o yut gsaddd sdewsawsaws ht gdse </p>
-      <p>welcome to our sitution gderwe sedcgfioyitkhftuo  yhghg going bt o yut gsaddd sdewsawsaws ht gdse </p>
-
+      <h1 className={styles.title}>Hello NextJs</h1>
+      <p className={styles.text}>welcome to our sitution gderwe sedcgfioyitkhftuo  yhghg going bt o yut gsaddd sdewsawsaws ht gdse </p>
+      <p className={styles.text}>welcome to our sitution gderwe sedcgfioyitkhftuo  yhghg going bt o yut gsaddd sdewsawsaws ht gdse </p>
+      <Link legacyBehavior href={'/mahad/test'}>
+        <a className={styles.btn}> See Mahad Listings</a>
+      </Link>
     </>
   )
 }
